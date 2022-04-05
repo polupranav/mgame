d9afa3f785a219cb30df0008ec50f022ece39aac
polupranav <87107493+polupranav@users.noreply.github.com>
2022-04-05 17:11:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav") ;
+	mario_coin = loadSound("coin.wav") ;
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wav");
+	setSprites();
+	MarioAnimation();
+
+}
+
+function setup() {
+	canvas = createCanvas(1240, 336);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	canvas.parent("canvas");
+
+	instializeInSetup(mario);
+
+	posenet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+	console.log("model loaded");
+}
+
+function gotPoses(results) {
+	if(results.length > 0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log(results);
+	}
+}
+
+function draw() {
+	game()
+}
